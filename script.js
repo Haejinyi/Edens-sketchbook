297470773a16c19d682a47e36103a180ac79272e
Hae Jin Jeong <haejinyi3233@gmail.com>
2022-01-09 10:28:12 +0900

    update success

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 
-
 const pencilModal = document.querySelector('.pencil');
 const modal = document.querySelector('.modal');
-
-// const nameInput = document.querySelector('.name');
-// const emailInput = document.querySelector('.email');
-// const messageInput = document.querySelector('.message');
-// const dateAtInput = document.querySelector('.date');
 
 const closeBtn = document.querySelector('.close-btn');
 const submitBtn = document.querySelector('.submit-btn');
@@ -15,25 +9,24 @@ const usersQueryObject = [];
 
 submitBtn.addEventListener('click', function(e) {
     // e.preventDefault();
+     
     const timeElapsed = Date.now();
     const today = new Date(timeElapsed);
     today.toDateString();
-
-
+ 
     const _newUserQuery = {
         Name: document.querySelector('.name').value,
         Email: document.querySelector('.email').value,
         Message: document.querySelector('.message').value,
         Date: today,
     }
-
-
+    
     usersQueryObject.push(_newUserQuery);
     console.log(usersQueryObject);
     document.querySelector('form').reset();
     modal.style.display = "none"
 
-    localStorage.setItem('MyGuestList', JSON.stringify(usersQueryObject));
+    localStorage.setItem('MyGuestList', JSON.stringify(usersQueryObject));  
 })
 
 pencilModal.addEventListener('click', function() {
